0e7e9f40f02d0863992f7ef3ddb04cd24262e8f8
Yagir <yagirman59@gmail.com>
2021-09-03 15:24:16 +0300

    Initial commit

---
+++ Assets/Cell.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Rotation { Left, Up, Right, Down};
+public enum CellKind { Sun, Meat, Combined};
+public class Cell
+{
+    Rotation rotation;
+    byte[] thoughts;
+    byte currentThought = 0;
+    byte thoughtsLength = 10;
+    Transform cellInWorld;
+    CellKind kind;
+    float energy;
+    World world;
+    public Cell(Transform cell, bool isRandomRotation = false, CellKind kind = CellKind.Sun)
+    {
+        this.kind = kind;
+        cellInWorld = cell;
+        cell.gameObject.SetActive(false);
+
+        thoughts = new byte[thoughtsLength];
+        for (int i = 0; i < thoughts.Length; i++)
+        {
+            thoughts[i] = (byte)Random.Range(0, 28);
+        }
+        currentThought = 0;
+        if (isRandomRotation) rotation = (Rotation)Random.Range(0, 4);
+
+        world = GameManager.instance.world;
+    }
+
+    public void WorldInit(Vector2Int pos)
+    {
+        cellInWorld.gameObject.SetActive(true);
+        cellInWorld.position = (Vector2)pos;
+    }
+
+
+    public void Brain()
+    {
+        if ((int)thoughts[currentThought] % 5 == 0)
+        {
+            DublicateCell();
+        }else if ((int)thoughts[currentThought] % 2 == 0)
+        {
+            MoveCell();
+        }
+        else if ((int)thoughts[currentThought] % 3 == 0)
+        {
+            RotateCell();
+        }
+
+        GoToNextThought();
+    }
+
+    public void GoToNextThought()
+    {
+        var n = thoughts[currentThought];
+        for (int i = 0; i < n; i++)
+        {
+            currentThought++;
+            if (currentThought > thoughtsLength)
+            {
+                currentThought = 0;
+            }
+        }
+    }
+
+    public void RotateCell()
+    {
+
+    }
+    public void DublicateCell()
+    {
+
+    }
+    public void MoveCell()
+    {
+
+    }
+
+
+    public void UpdateCell()
+    {
+        switch (kind)
+        {
+            case CellKind.Sun:
+                SunCellUpdate();
+                break;
+            case CellKind.Meat:
+                MeatCellUpdate();
+                break;
+            case CellKind.Combined:
+                MeatCellUpdate();
+                break;
+            default:
+                break;
+        }
+        Brain();
+    }
+    public void SunCellUpdate()
+    {
+        energy += world.sunEnergy;
+        if ((int)energy > world.sunEnergy)
+        {
+            energy = world.sunEnergy;
+        }
+    }
+    public void MeatCellUpdate()
+    {
+
+    }
+    public void CombinedCellUpdate()
+    {
+        SunCellUpdate();
+        MeatCellUpdate();
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class World {
+    public float sunEnergy;
+    public float rotateEnergy;
+    public float moveEnergy;
+    public int maxEnergy;
+}
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] int fieldSize;
+    Cell[,] cells;
+    [SerializeField] GameObject cellPrefab;
+    List<Cell> cellsPool = new List<Cell>();
+    public World world;
+
+    private void Awake()
+    {
+        instance = this;
+        PoolInit();
+        Init();
+
+        StartCoroutine(Ticks());
+    }
+
+    public void PoolInit()
+    {
+        for (int i = 0; i < fieldSize * fieldSize; i++)
+        {
+            var inst = Instantiate(cellPrefab, transform);
+            Cell cell = new Cell(inst.transform);
+            cellsPool.Add(cell);
+        }
+    }
+    public Cell GetFromPool()
+    {
+        if (cellsPool.Count > 0)
+        {
+            var cell = cellsPool[0];
+            cellsPool.RemoveAt(0);
+            return cell;
+        }
+        return null;
+    }
+    public void Init()
+    {
+        cells = new Cell[fieldSize, fieldSize];
+        var pos = new Vector2Int(Random.Range(0, fieldSize), Random.Range(0, fieldSize));
+        Set(pos, GetFromPool());
+        Get(pos).WorldInit(pos);
+    }
+    public Cell Get(Vector2Int pos)
+    {
+        return cells[pos.x, pos.y];
+    }
+    public Cell Set(Vector2Int pos, Cell cell)
+    {
+        cells[pos.x, pos.y] = cell;
+        return cells[pos.x, pos.y];
+    }
+
+    IEnumerator Ticks()
+    {
+        while (true)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int x = 0; x < cells.GetLength(0); x++)
+                {
+                    for (int y = 0; y < cells.GetLength(1); y++)
+                    {
+                        if (cells[x, y] != null)
+                            cells[x, y].UpdateCell();
+                    }
+                }
+                yield return new WaitForSeconds(1f / 20f);
+            }
+            for (int x = 0; x < cells.GetLength(0); x++)
+            {
+                for (int y = 0; y < cells.GetLength(1); y++)
+                {
+                    if (cells[x, y] != null)
+                        cells[x, y].Brain();
+                }
+            }
+        }
+    }
+
+}
